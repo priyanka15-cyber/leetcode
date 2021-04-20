b7b7f68db8677024e167b02832f5853fa38983ff
Priyanka Kumari <67923478+priyanka15-cyber@users.noreply.github.com>
2021-04-21 01:34:10 +0530

    Create Target Array in the Given Order
    Atlassian asked question :(

---
+++ easy/1389.cpp
@@ -0,0 +1,8 @@
+ vector<int> createTargetArray(vector<int>& nums, vector<int>& index) 
+    {
+        vector<int> v;
+        for(int i=0;i<nums.size();i++)
+            v.insert(v.begin()+index[i],nums[i]);   
+        return v;
+            
+    }
